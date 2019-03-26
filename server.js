0883a5bb72e3bf01b904fda82fe98ee663170374
Owen Stephens <owen@ostephens.com>
2019-03-26 09:24:29 +0000

    Started to parameterise for pagination

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const request = require('request');
 const express = require('express');
+const paginate = require('express-paginate');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(paginate.middleware(100, 200));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
+  var limit = 10;
+  var offset = 0;
   if (typeof req.query.ringgold !== "undefined") {
     if (typeof req.query.limit !== "undefined") {
-      var limit = req.query.limit;
+      limit = req.query.limit;
     }
     if (typeof req.query.offset !== "undefined") {
-      var offset = req.query.offset;
+      offset = req.query.offset;
     }
-    var u = 'https://pub.orcid.org/v2.0/search/?q=ringgold-org-id:'+ req.query.ringgold;
+    // ORCiD API Search query params:
+    // q = query
+    // start = first record to return (defaults to 1)
+    // rows = number of records to return (defaults to 100, max 200)
+    var u = 'https://pub.orcid.org/v2.0/search/?q=ringgold-org-id:'+
+             req.query.ringgold +
+             '&start=' + offset +
+             '&rows=' + limit;
     //to do local testing uncomment the next line
     //var u = "http://localhost:3000/orcid-search-response"
+    console.log(u);
     var n = "Test";  
     var options = {
       url: u,
