d05a1490d08a24df6f5c6ad732ea7dcd6ebd0dd3
Owen Stephens <owen@ostephens.com>
2019-03-26 13:58:56 +0000

    Adding pagination

--- server.js
+++ server.js
@@ -4,29 +4,21 @@ const paginate = require('express-paginate');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(paginate.middleware(100, 200));
+app.use(paginate.middleware(10, 200));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  var limit = 10;
-  var offset = 0;
   if (typeof req.query.ringgold !== "undefined") {
-    if (typeof req.query.limit !== "undefined") {
-      limit = req.query.limit;
-    }
-    if (typeof req.query.offset !== "undefined") {
-      offset = req.query.offset;
-    }
     // ORCiD API Search query params:
     // q = query
     // start = first record to return (defaults to 1)
     // rows = number of records to return (defaults to 100, max 200)
     var u = 'https://pub.orcid.org/v2.0/search/?q=ringgold-org-id:'+
              req.query.ringgold +
-             '&start=' + offset +
-             '&rows=' + limit;
+             '&start=' + (req.query.page-1)*req.query.limit +
+             '&rows=' + req.query.limit;
     //to do local testing uncomment the next line
     //var u = "http://localhost:3000/orcid-search-response"
     console.log(u);
@@ -40,7 +32,7 @@ app.get('/', function (req, res) {
     
     //set default variables
    var totalOrcids = 0,
-   pageSize = 100,
+   pageSize = 10,
    currentPage = 1,
    orcidsList = [];
    
@@ -48,10 +40,12 @@ app.get('/', function (req, res) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
         n = info["num-found"];
+        const itemCount = n;
+        const pageCount = Math.ceil(n / req.query.limit);
         console.log("Query found " + n + " ORCiD IDs");
         orcidsList = info.result;
         //console.log(orcidsList);
-        res.render('index', {count: n, orcids: orcidsList, pageCount: 0, currentPage: currentPage, error: null});
+        res.render('index', {count: n, orcids: orcidsList, pageCount: pageCount, itemCount: itemCount, pages: paginate.getArrayPages(req)(3, pageCount, req.query.page), error: null});
       }
     }
     request(options, callback);
