4b3cc7be2c1e76ddc560def52ad0744d3a5d8c8e
Owen Stephens <owen@ostephens.com>
2021-10-28 17:48:17 +0100

    Single search box

--- public/js/script.js
+++ public/js/script.js
@@ -4,12 +4,13 @@ function selectRegion() {
   // Turn display off for all regions
   document.getElementById('us-form').style.display = 'none';
   document.getElementById('uk-form').style.display = 'none';
-  
+
   // Turn on selected region
   document.getElementById(regionSelection).style.display = 'block';
 }
 
 function selectBoxes() {
+  /*
   new TomSelect('#ringgold-identifiers',{
     maxItems: 1,
     valueField: 'id',
@@ -44,7 +45,7 @@ function selectBoxes() {
     create: true,
     createFilter: '(.+\\|?)+'
   });
-  
+  */
   new TomSelect('#ror-api',{
   valueField: 'idList',
   labelField: 'name',
@@ -60,18 +61,37 @@ function selectBoxes() {
             ror: item.id,
             grid: item.external_ids.GRID.preferred
           }
+          let idMapping = idMappings.find(inst => inst.ror === item.id);
+          if(idMapping != undefined) {
+            idList.ringgold = idMapping.ringgold
+            idList.domains = idMapping.domains
+            idList.name = idMapping.name
+          }
           let i = {
             id: item.id,
             name: item.name,
             idList: JSON.stringify(idList)
           }
-          console.log(i);
+          //console.log(i);
           return i
         })
         callback(data);
       }).catch(()=>{
         callback();
       });
-    }
+    },
+  render: {
+    item: function(item, escape) {
+  				return `<div>
+                  <span class="h4">
+  									${ escape(item.name) }
+  								</span>
+  								<div class="text-muted">ROR: ${ JSON.parse(item.idList).ror }</div>
+                  <div class="text-muted">GRID: ${ JSON.parse(item.idList).grid }</div>
+                  <div class="text-muted">Ringgold: ${ JSON.parse(item.idList).ringgold }</div>
+                  <div class="text-muted">Email domains: ${ JSON.parse(item.idList).domains }</div>
+    						</div>`;
+  			}
+  }
   });
 }
